0efea742a21d1e83a7b7f0678d6ce3ace68b6c0a
Kauthar M. Omar <57720624+Kauthar-Omar@users.noreply.github.com>
2022-10-09 06:14:35 +0300

    Update 02_quality_control.sh

--- scripts/02_quality_control.sh
+++ scripts/02_quality_control.sh
@@ -1,10 +1,17 @@
 #Perform Fastqc
+
+#Data directories
+ke_data_dir="../data/ke_data"
+tz_data_dir="../data/tz_data"
+ug_data_dir="../data/ug_data"
+
+
 #running fastqc on Kenyan dataset
-fastqc ../data/ke_data/* -o ke_fastqc_results
+fastqc $ke_data_dir -o ke_fastqc_results
 #running fastqc on Ugandan dataset
-fastqc ../data/ug_data/* -o ug_fastqc_results
+fastqc $tz_data_dir -o ug_fastqc_results
 #running fastqc on Tanzanian dataset
-fastqc ../data/tz_data/* -o tz_fastqc_results
+fastqc $ug_data_dir -o tz_fastqc_results
 
 #Perform multiqc
 #running multiqc on Kenyan dataset
